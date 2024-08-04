0d5c5ef6f0ba3c813941533d94f81608e941088c
Cz_Analista <cz@cz.cz>
2024-08-03 21:18:30 -0400

    intentando añadir botón _to top_

--- nutribite-login/src/App.js
+++ nutribite-login/src/App.js
@@ -1,24 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import HomePage from './HomePage'; // Asegúrate de crear este componente para tu página principal
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/ingreso" component={LoginForm} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
