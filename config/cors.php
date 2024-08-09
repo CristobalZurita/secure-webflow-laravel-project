529ecc95225777829938226af4939a582ae9b799
Cz_Analista <cz@cz.cz>
2024-08-09 06:16:42 -0400

    INICIO NUEVAMENTE

--- config/cors.php
+++ config/cors.php
@@ -1,10 +1,12 @@
+<?php
+
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:8000'], // Ajusta esto según tu configuración
+    'allowed_origins' => ['http://localhost:8000'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 0,
+    'max_age' => 0,  // No necesitas forzar el casting a int aquí
     'supports_credentials' => true,
 ];
